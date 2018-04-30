ba27f52c716be8092004905b380e5f91abd4b15a
Andy Bennett <andy@firef.ly>
2018-04-30 11:22:25 +0100

    Initial Commit

---
+++ Fruit/Fruit.h
@@ -0,0 +1,19 @@
+//
+//  Fruit.h
+//  Fruit
+//
+//  Created by Andy Bennett on 30/04/2018.
+//  Copyright © 2018 Andy Bennett. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Fruit.
+FOUNDATION_EXPORT double FruitVersionNumber;
+
+//! Project version string for Fruit.
+FOUNDATION_EXPORT const unsigned char FruitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Fruit/PublicHeader.h>
+
+
---
+++ FruitTests/FruitTests.m
@@ -0,0 +1,39 @@
+//
+//  FruitTests.m
+//  FruitTests
+//
+//  Created by Andy Bennett on 30/04/2018.
+//  Copyright © 2018 Andy Bennett. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface FruitTests : XCTestCase
+
+@end
+
+@implementation FruitTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
